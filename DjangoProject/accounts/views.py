129266c8397c2c57107bec3ca372642a87a686f1
Alida <hjelljordalida@gmail.com>
2021-11-03 20:20:23 +0100

    Lesson 21, 22, 23: login/logout

--- DjangoProject/accounts/views.py
+++ DjangoProject/accounts/views.py
@@ -1,16 +1,35 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
-# Create your views here.
-
+# view for signup form, if successfull logs in and redirects to articles
 def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            #Log the user inn
+            user = form.save()
+            login(request, user)
             return redirect('articles:list')
     else:
         form = UserCreationForm()
     
     return render(request, 'accounts/signup.html', {'form':form})
+
+# view for login form, if successfull logs in and redirects to articles
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('articles:list') 
+    else:
+        form = AuthenticationForm()
+
+    return render(request,'accounts/login.html',{'form':form})
+
+# method for logout if 
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('articles:list')
